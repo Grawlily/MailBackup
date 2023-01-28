bc4b192b76c48c86bfe74a3539ad342141b5692a
grarich <grarich@grawlily.com>
2023-01-29 00:50:50 +0900

    fix: コネクション接続忘れを修正

--- lib/utils/check_imap_connection.dart
+++ lib/utils/check_imap_connection.dart
@@ -7,17 +7,25 @@ Future<bool> checkImapConnection(String host, int port, String userName,
     String password, bool isSecure) async {
   final client = ImapClient(isLogEnabled: false);
   try {
-    await client.connectToServer(host, port, isSecure: isSecure);
+    await client.connectToServer(host, port,
+        isSecure: isSecure, timeout: const Duration(seconds: 5));
     if (!isSecure) {
       await client.startTls();
     }
     await client.login(userName, password);
     await client.logout();
+    await client.disconnect();
     return true;
   } on ImapException catch (e) {
     if (kDebugMode) print(e);
     return false;
   } on SocketException catch (e) {
+    if (kDebugMode) print(e);
+    return false;
+  } on HandshakeException catch (e) {
+    if (kDebugMode) print(e);
+    return false;
+  } catch (e) {
     if (kDebugMode) print(e);
     return false;
   }
