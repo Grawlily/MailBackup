68513e0ae22f08929535f92ea3a28f88c6f4c7ca
grarich <grarich@grawlily.com>
2023-01-29 01:03:01 +0900

    fix: 取得設定を修正

--- lib/utils/preserve_mails.dart
+++ lib/utils/preserve_mails.dart
@@ -1,11 +1,10 @@
 import 'dart:io';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart';
 import "package:intl/intl.dart";
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:mail_backup/utils/check_imap_connection.dart';
-import 'package:mail_backup/repositories/shared_preferences_util.dart';
+import 'package:path/path.dart';
 import 'package:enough_mail/enough_mail.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'package:mail_backup/utils/check_imap_connection.dart';
 
 class NotConnectable implements Exception {
   NotConnectable();
@@ -14,54 +13,52 @@ class NotConnectable implements Exception {
 Stream<List<MimeMessage>> pagedMessageStream(
     ImapClient client, Mailbox box) async* {
   int pageSize = (box.messagesExists / 500).ceil();
-  for (var i = 0; i < pageSize; i++) {
+  for (int i = 1; i < pageSize + 1; i++) {
     MessageSequence sequence =
         MessageSequence.fromPage(i, pageSize, box.messagesExists);
-    FetchImapResult result = await client.fetchMessages(sequence, null);
+    FetchImapResult result =
+        await client.fetchMessages(sequence, 'BODY.PEEK[]');
     yield result.messages;
   }
 }
 
-Future<String> preserveMail(MimeMessage message, Mailbox box) async {
-  Directory baseDir = await getApplicationDocumentsDirectory();
-  DateFormat dateFormat = DateFormat('yyyyMMdd-HHmm');
-  String backupDirPath = join(baseDir.path, dateFormat.format(DateTime.now()));
-
+Future<String> preserveMail(
+    MimeMessage message, Mailbox box, String backupDirPath) async {
   File file =
-      await File(join(backupDirPath, box.name, '${message.internalDate}.mlbk'))
-          .create();
+      await File(join(backupDirPath, box.name, '${message.hashCode}.mlbk'))
+          .create(recursive: true);
   await file.writeAsString(message.toString());
   return file.path;
 }
 
-Future<void> preserveMails() async {
+Future<void> preserveMails(String host, int port, String userName,
+    String password, bool isSecure) async {
+  Directory baseDir = await getApplicationDocumentsDirectory();
+  DateFormat dateFormat = DateFormat('yyyyMMdd-HHmm');
+  String backupDirPath = join(baseDir.path, dateFormat.format(DateTime.now()));
+
   ImapClient client = ImapClient();
-  SharedPreferences sp = await SharedPreferences.getInstance();
-  String host = getStringSafe(sp, 'host');
-  int port = getIntSafe(sp, 'port');
-  bool isSecure = getBoolSafe(sp, 'isSecure');
-  String username = getStringSafe(sp, 'userName');
-  String password = getStringSafe(sp, 'password');
 
   final connectable =
-      await checkImapConnection(host, port, username, password, isSecure);
+      await checkImapConnection(host, port, userName, password, isSecure);
   if (!connectable) {
     throw NotConnectable();
   }
 
   await client.connectToServer(host, port, isSecure: isSecure);
 
-  if (isSecure) {
+  if (!isSecure) {
     await client.startTls();
   }
 
-  await client.login(username, password);
+  await client.login(userName, password);
 
   List<Mailbox> mailboxes = await client.listMailboxes();
   for (Mailbox box in mailboxes) {
+    await client.selectMailbox(box);
     await for (List<MimeMessage> messages in pagedMessageStream(client, box)) {
       for (MimeMessage m in messages) {
-        await preserveMail(m, box);
+        await preserveMail(m, box, backupDirPath);
       }
     }
   }
