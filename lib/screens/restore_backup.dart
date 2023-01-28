e6e42c5405d545d478e44dfbb7d4bf19ed00488e
grarich <grarich@grawlily.com>
2023-01-29 01:01:18 +0900

    Revert "fix: 取得設定を修正"
    This reverts commit 85543a7e7e8daf38b40bf35993e91f4be6367681.

--- lib/screens/restore_backup.dart
+++ lib/screens/restore_backup.dart
@@ -19,12 +19,11 @@ class Main extends StatefulWidget {
 class MainState extends State<Main> {
   @override
   Widget build(BuildContext context) {
-    return _body();
+    return  _body();
   }
 
   Widget _body() {
     return Center(
-
     );
   }
 }
