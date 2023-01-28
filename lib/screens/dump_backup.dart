de6f033bb5b06b448b9c606e34bfb725517f34e3
grarich <grarich@grawlily.com>
2023-01-29 02:51:55 +0900

    feat: バックアップ完了のポップアップの色を修正

--- lib/screens/dump_backup.dart
+++ lib/screens/dump_backup.dart
@@ -1,4 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:mail_backup/repositories/app_config.dart';
+import 'package:mail_backup/utils/compress.dart';
+import 'package:mail_backup/utils/preserve_mails.dart';
+
+import 'package:mail_backup/utils/shared_preferences_util.dart';
+import 'package:path/path.dart' as pathlib;
+import 'package:path_provider/path_provider.dart';
 
 class Backup extends StatelessWidget {
   const Backup({super.key});
@@ -17,13 +26,119 @@ class Main extends StatefulWidget {
 }
 
 class MainState extends State<Main> {
+  bool _backupProcessing = false;
+
   @override
   Widget build(BuildContext context) {
     return _body();
   }
 
   Widget _body() {
+    return _backupProcessing ? _processing() : _noProcessing();
+  }
+
+  Widget _noProcessing() {
     return Center(
+      child: OutlinedButton(
+        onPressed: () async {
+          await _backupMails();
+        },
+        style: OutlinedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50),
+          ),
+        ),
+        child: Text(
+          'メールをバックアップ',
+          style: TextStyle(
+              fontSize: MediaQuery.of(context).size.shortestSide * 0.05),
+        ),
+      ),
+    );
+  }
+
+  Widget _processing() {
+    return Center();
+  }
+
+  Future<void> _backupMails() async {
+    setState(() {
+      _backupProcessing = true;
+    });
+    try {
+      AppConfig config = await AppConfig.getInstance();
+      String outDir = await preserveMails(config.getHost(), config.getPort(),
+          config.getUserName(), config.getPassword(), config.getIsSecure());
+      Directory directory = await getApplicationDocumentsDirectory();
+      if (!await directory.exists()) {
+        await directory.create(recursive: true);
+      }
+      await compress(outDir, '${pathlib.basename(outDir)}.zip');
+    } on PreferenceNotFound catch (e) {
+      return _showError(e);
+    } on NotConnectable catch (e) {
+      return _showError(e);
+    } catch (e) {
+      return _showError(e);
+    }
+    await showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (_) {
+        return AlertDialog(
+          title: const Text('バックアップが終了しました。'),
+          icon: Icon(
+            Icons.check_outlined,
+            color: Colors.green,
+            size: MediaQuery.of(context).size.shortestSide * 0.1,
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                'OK',
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                ),
+              ),
+              onPressed: () => Navigator.of(context).pop(0),
+            ),
+          ],
+        );
+      },
+    );
+    setState(() {
+      _backupProcessing = false;
+    });
+  }
+
+  Future<void> _showError(Object e) async {
+    late String message;
+    if (e is PreferenceNotFound) {
+      message = 'メールサーバーの設定を行ってください。';
+    } else if (e is NotConnectable) {
+      message = 'メールサーバーに接続できませんでした。';
+    } else {
+      message = '予期しないエラーが発生しました。';
+    }
+    double iconSize = MediaQuery.of(context).size.shortestSide * 0.1;
+    return await showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (_) {
+        return AlertDialog(
+          title: Text(message),
+          icon: Icon(Icons.error_outline, color: Colors.red, size: iconSize),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                'OK',
+                style: TextStyle(color: Colors.blueAccent),
+              ),
+              onPressed: () => Navigator.of(context).pop(0),
+            ),
+          ],
+        );
+      },
     );
   }
 }
